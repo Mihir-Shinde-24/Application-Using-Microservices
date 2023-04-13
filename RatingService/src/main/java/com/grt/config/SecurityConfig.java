98094c1061fc3c1e5b0c5fd5963d43c98537e4b1
Mihir Shinde <mihir.s@grtship.com>
2023-04-13 15:51:00 +0530

    ss in rating and hotel error

---
+++ RatingService/src/main/java/com/grt/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.grt.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+	
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
+	{
+		http.authorizeHttpRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
+		
+		return http.build();
+	}
+}
