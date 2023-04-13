f00924f325f93d0467043408043ee9802326ba5c
Mihir Shinde <mihir.s@grtship.com>
2023-04-13 23:59:51 +0530

    pw root in yml added

--- UserService/src/main/java/com/grt/config/WebSecurityConfig.java
+++ UserService/src/main/java/com/grt/config/WebSecurityConfig.java
@@ -1,15 +1,18 @@
 package com.grt.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
-	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
 	{
 		http.authorizeHttpRequests()
